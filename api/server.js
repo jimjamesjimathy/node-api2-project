8b73bc974739d93f7aa822714240fae52567fb74
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-02-08 12:55:15 -0500

    All MVP tasks completed!

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+const server = express();
+
+server.use(express.json());
+
 // require your posts router and connect it here
+server.use('/api/posts', postsRouter)
+server.use('*', (req, res) => {
+    res.status(404).json({message: 'not here, buddy.'})
+});
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server');
+const port = 8080;
+
+server.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
